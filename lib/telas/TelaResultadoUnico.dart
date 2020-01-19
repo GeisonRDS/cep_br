a40f1ddbdb28799afdfded2b77a5b4eae3307c9a
GeisonRDS <geisonrobertodasilva@gmail.com>
2020-01-19 12:57:28 -0300

    Implementado Future na tela TelaResultadoUnico e validação de estado de conexão.

--- lib/telas/TelaResultadoUnico.dart
+++ lib/telas/TelaResultadoUnico.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cep/model/Cep.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -17,46 +18,27 @@ class _TelaResultadoUnicoState extends State<TelaResultadoUnico> {
 
   String _urlBase = "https://viacep.com.br/ws/";
 
-  String _mensagemResultado = "";
+  Future<Cep> _recuperarEnderecos() async {
 
-  void _recuperarCep() async {
+    String url = _urlBase+widget.pesquisaDigitada+"/json/";
 
-    String url = "https://viacep.com.br/ws/${widget.pesquisaDigitada}/json/";
+    http.Response response = await http.get( url );
+    var dadosJson = json.decode( response.body );
 
-    print(url);
-
-    http.Response response;
-    response = await http.get(url);
-    Map<String, dynamic> retorno = json.decode(response.body);
-    String cepDigitado = retorno["cep"]         == "" ? "" : "CEP: ${retorno["cep"]}\n";
-    String logradouro  = retorno["logradouro"]  == "" ? "" : "Logradouro: ${retorno["logradouro"]}\n";
-    String complemento = retorno["complemento"] == "" ? "" : "Complemento: ${retorno["complemento"]}\n";
-    String bairro      = retorno["bairro"]      == "" ? "" : "Bairro: ${retorno["bairro"]}\n";
-    String localidade  = retorno["localidade"]  == "" ? "" : "Cidade: ${retorno["localidade"]}\n";
-    String uf          = retorno["uf"]          == "" ? "" : "UF: ${retorno["uf"]}\n";
-    String unidade     = retorno["unidade"]     == "" ? "" : "Unidade: ${retorno["unidade"]}\n";
-    String ibge        = retorno["ibge"]        == "" ? "" : "IBGE: ${retorno["ibge"]}\n";
-    String gia         = retorno["gia"]         == "" ? "" : "GIA: ${retorno["gia"]}";
-
-    setState(() {
-      _mensagemResultado = "${cepDigitado}"
-          + "${logradouro}"
-          + "${complemento}"
-          + "${bairro}"
-          + "${localidade}"
-          + "${uf}"
-          + "${unidade}"
-          + "${ibge}"
-          + "${gia}";
-    });
+    var cep = dadosJson;
+    if(dadosJson.toString() == "{erro: true}"){
+      Cep c = Cep(cep["cep"]="", cep["logradouro"]="", cep["complemento"]="", cep["bairro"]="", cep["localidade"]="", cep["uf"]="", cep["unidade"]="", cep["ibge"]="", cep["gia"]="");
+      return c;
+    }else{
+      Cep c = Cep(cep["cep"], cep["logradouro"], cep["complemento"], cep["bairro"], cep["localidade"], cep["uf"], cep["unidade"], cep["ibge"], cep["gia"]);
+      return c;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
 
-    _recuperarCep();
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         iconTheme: IconThemeData(
             color: Colors.blue
@@ -64,26 +46,61 @@ class _TelaResultadoUnicoState extends State<TelaResultadoUnico> {
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-      body: Center(
-          child: Padding(
-              padding: EdgeInsets.fromLTRB(20, 0, 20, 60),
-              child: Container(
-                padding: EdgeInsets.only(
-                    left: 50, top: 30, right: 50, bottom: 30),
-                decoration: BoxDecoration(
-                    border: Border(top: BorderSide(color: Colors.blue),
-                        bottom: BorderSide(color: Colors.blue))
-                ),
-                child: Text(_mensagemResultado,
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue
-                    )
-                ),
-              )
-          )
-      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: EdgeInsets.only(left:20, right: 20),
+          child: Center(
+            child: FutureBuilder<Cep>(
+              future: _recuperarEnderecos(),
+              builder: (context, snapshot){
+                switch( snapshot.connectionState ){
+                  case ConnectionState.none :
+                  case ConnectionState.waiting :
+                    return Container(
+                      child: CircularProgressIndicator(),
+                    );
+                    break;
+                  case ConnectionState.active :
+                  case ConnectionState.done :
+                    if( snapshot.hasError || snapshot.data.cep.isEmpty){
+                      return Center(
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                                  child: Text("Não foram encontrados registros com os dados informados.",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blue
+                                    ),
+                                  ),
+                                )
+                              );
+                    }else {
+                      Cep cep = snapshot.data;
+                      return Text("\n${cep.cep == ""?"":"CEP: "+cep.cep+"\n"}"
+                            "${cep.logradouro == ""?"":"Logradouro: "+cep.logradouro+"\n"}"
+                            "${cep.complemento == ""?"":"Complemento: "+cep.complemento+"\n"}"
+                            "${cep.bairro == ""?"":"Bairro: "+cep.bairro+"\n"}"
+                            "${cep.localidade == ""?"":"Cidade: "+cep.localidade+"\n"}"
+                            "${cep.uf == ""?"":"UF: "+cep.uf+"\n"}"
+                            "${cep.unidade == ""?"":"Unidade: "+cep.unidade+"\n"}"
+                            "${cep.ibge == ""?"":"IBGE: "+cep.ibge+"\n"}"
+                            "${cep.gia == ""?"":"GIA: "+cep.gia}",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue
+                          ),
+                        );
+                      }
+                    break;
+                  }
+                }
+            ),
+          ),
+        ),
+      )
     );
   }
 }
